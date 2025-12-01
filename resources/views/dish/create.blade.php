4a0b3c59aee340a4639de481c1697b14e2a2854e
Adrian JG <jesusadrianjeronimo113@gmail.com>
2025-12-01 14:00:15 -0600

    add dish folder with create.html and index.html

---
+++ resources/views/dish/create.blade.php
@@ -0,0 +1,38 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2>Create view</h2>
+    </x-slot>
+    <div class="flex justify-center mt-10">
+        <div class="card w-96 bg-base-100 shadow-xl">
+            <div class="card-body">
+                <form action="{{ route('dish.store') }}" method="POST">
+                    @csrf
+                    <label class="input m-2">
+                        <span class="label">Nombre</span>
+                        <input class="text-black" type="text" placeholder="Enchiladas Verde" name="name" />
+                    </label>
+
+                    <label class="input m-2">
+                        <span class="label">Descripcion</span>
+                        <input class="text-black" type="text" placeholder="Ingredientes" name="description" />
+                    </label>
+
+                    <label class="input m-2">
+                        <span class="label">Precio</span>
+                        <input class="text-black" type="text" placeholder="100" name="price"/>
+                    </label>
+
+                    <label class="select">
+                        <span class="label">Categoria</span>
+                        <select class="text-black">
+                            <option>Personal</option>
+                            <option>Business</option>
+                        </select>
+                    </label>
+                    <button class="btn btn-primary mt-4 w-full">Guardar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</x-app-layout>
---
+++ resources/views/dish/index.blade.php
@@ -0,0 +1,41 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ ('Entradas') }}
+        </h2>
+    </x-slot>
+    <button class=" glass border border-blue-600 text-white hover:bg-blue-600 hover:text-white font-medium px-4 py-2 rounded-md transition m-3">
+        <a href="{{ route('dish.create') }}">Agregar</a>
+    </button>
+
+    <div class="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+        <table class="table">
+            <!-- head -->
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Descripcion</th>
+                    <th>Precio</th>
+                    <th>Categoria</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($dishes as $dish)
+                    <tr>
+                        <th>hola</th>
+                        <th>hola</th>
+                        <th>hola</th>
+                        <th>hola</th>
+                    </tr>
+                @empty
+                    <tr>
+                        <th>No data</th>
+                        <th>No data</th>
+                        <th>No data</th>
+                        <th>No data</th>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
+</x-app-layout>
