1ba4053c7e0421051d58afbeff74bf13da886279
Adrian JG <jesusadrianjeronimo113@gmail.com>
2025-12-02 22:28:41 -0600

    add helper: old

--- resources/views/dish/edit.blade.php
+++ resources/views/dish/edit.blade.php
@@ -6,37 +6,41 @@
     <div class="flex justify-center mt-10">
         <div class="card w-96 bg-base-100 shadow-xl">
             <div class="card-body">
-                <form action="{{ route('dish.update', $dish->id)}}" method="POST" enctype="multipart/form-data">
+                <form action="{{ route('dish.update', $dish->id) }}" method="POST" enctype="multipart/form-data">
                     @csrf
-                     @method('PATCH')
+                    @method('PATCH')
                     <label class="input m-2">
                         <span class="label">Nombre</span>
-                        <input class="text-black" type="text" placeholder="Enchiladas Verde" name="name" value="{{ $dish->name }}"/>
+                        <input class="text-black" type="text" placeholder="Enchiladas" name="name"
+                            value="{{ old('name', $dish->name) }}" />
                     </label>
 
                     <label class="input m-2">
                         <span class="label">Descripcion</span>
-                        <input class="text-black" type="text" placeholder="Ingredientes" name="description" value="{{ $dish->description }}"/>
+                        <input class="text-black" type="text" placeholder="Ingredientes" name="description"
+                            value="{{ old('description', $dish->description) }}" />
                     </label>
 
                     <label class="input m-2">
                         <span class="label">Precio</span>
-                        <input class="text-black" type="text" placeholder="100" name="price" value="{{ $dish->price }}"/>
+                        <input class="text-black" type="text" placeholder="100" name="price"
+                            value="{{ old('price', $dish->price) }}" />
                     </label>
 
                     <label class="select">
                         <span class="label">Categoria</span>
                         <select class="text-black" name="category_id">
-                            @forelse ($categories as $category)
-                                <option value="{{ $category->id }}" {{ $category->id == $dish->category_id ? 'selected' : '' }}>
-                                    {{ $category->name}}
+                            @forelse ($categories as $name => $id)
+                                <option {{ old('category_id', $dish->category_id) == $id ? 'selected' : '' }}
+                                    value="{{ $id }}">
+                                    {{ $name }}
                                 </option>
                             @empty
                                 <option>No options</option>
                             @endforelse
                         </select>
                     </label>
-                    <button  type="submit" class="btn btn-primary mt-4 w-full">Guardar</button>
+                    <button type="submit" class="btn btn-primary mt-4 w-full">Guardar</button>
                 </form>
             </div>
         </div>
