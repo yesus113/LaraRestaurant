4c77ee33bdb8e116033d1f869326fb1a350d8ff8
Adrian JG <jesusadrianjeronimo113@gmail.com>
2025-12-02 22:25:52 -0600

    add helper old

--- resources/views/dish/create.blade.php
+++ resources/views/dish/create.blade.php
@@ -9,30 +9,35 @@
                     @csrf
                     <label class="input m-2">
                         <span class="label">Nombre</span>
-                        <input class="text-black" type="text" placeholder="Enchiladas Verde" name="name" />
+                        <input class="text-black" type="text" placeholder="Enchiladas Verde" name="name"
+                            value="{{ old('name') }}" />
                     </label>
 
                     <label class="input m-2">
                         <span class="label">Descripcion</span>
-                        <input class="text-black" type="text" placeholder="Ingredientes" name="description" />
+                        <input class="text-black" type="text" placeholder="Ingredientes" name="description"
+                            value="{{ old('description') }}" />
                     </label>
 
                     <label class="input m-2">
                         <span class="label">Precio</span>
-                        <input class="text-black" type="text" placeholder="100" name="price"/>
+                        <input class="text-black" type="text" placeholder="100" name="price"
+                            value="{{ old('price') }}" />
                     </label>
 
                     <label class="select">
                         <span class="label">Categoria</span>
                         <select class="text-black" name="category_id">
                             @forelse ($categories as $category)
-                                <option value="{{ $category->id }}">{{ $category->name}}</option>
+                                <option {{ old('category_id') == $category->id ? 'selected' : '' }} value="{{ $category->id }}">
+                                    {{ $category->name }}
+                                </option>
                             @empty
                                 <option>No options</option>
                             @endforelse
                         </select>
                     </label>
-                    <button  type="submit" class="btn btn-primary mt-4 w-full">Guardar</button>
+                    <button type="submit" class="btn btn-primary mt-4 w-full">Guardar</button>
                 </form>
             </div>
         </div>
