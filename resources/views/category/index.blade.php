0f6e393b1505d4ee58bcef1974bab15cef8a53c3
Emmanuel Ramirez <161186127+manolitobes@users.noreply.github.com>
2025-12-27 23:42:41 -0600

    modified: se agrega componente button

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -2,7 +2,11 @@
     <x-slot name="header">
         <h1>Categorias</h1>
     </x-slot>
+    @component('components.buttons.button-create')
+    @section('ruta')
+        <a href="{{ route('dish.create') }}">Agregar</a>
+    @endsection
+    @endcomponent
 
-    
 </x-app-layout>
 
