f8bc0a62429188d4e95f6dec017a3855ea3dc7a9
Emmanuel Ramirez <161186127+manolitobes@users.noreply.github.com>
2025-12-28 01:11:31 -0600

    modified: se actualiza el index con componentes

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -3,10 +3,45 @@
         <h1>Categorias</h1>
     </x-slot>
     @component('components.buttons.button-create')
-    @section('ruta')
-        <a href="{{ route('dish.create') }}">Agregar</a>
-    @endsection
+        @section('ruta')
+            <a href="{{ route('categ.create') }}">Agregar</a>
+        @endsection
     @endcomponent
 
+    <div class="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+        <table class="table">
+            <!-- head -->
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($categories as $dish)
+                    <tr>
+                        <th>{{ $dish->name }}</th>
+                        <th>
+                            @component('components.buttons.button-edit')
+                                @section('ruta1')
+                                    <a href="{{ route('dish.edit', $dish->id) }}">Editar</a>
+                                @endsection
+                            @endcomponent
+                            @component('components.buttons.button-delete')
+                                @section('route-del')
+                                    <form method="POST" action="{{ route('dish.destroy', $dish->id) }}">
+                                    @endsection
+                                @endcomponent
+                        </th>
+                    </tr>
+                @empty
+                    <tr>
+                        <th>No data</th>
+                        <th>No data</th>
+                        <th>No data</th>
+                        <th>No data</th>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
 </x-app-layout>
-
