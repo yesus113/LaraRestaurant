36b770f3260009b42d7dc0c77ce63213967f1b98
Adrian JG <jesusadrianjeronimo113@gmail.com>
2025-12-28 13:25:29 -0600

    modified title to 'Products'

--- resources/views/dish/index.blade.php
+++ resources/views/dish/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight"> {{ 'Entradas' }}</h2>
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight"> {{ 'Products' }}</h2>
     </x-slot>
 
     <x-buttons.button-create route="{{ route('dish.create') }}" name="Crear" />
