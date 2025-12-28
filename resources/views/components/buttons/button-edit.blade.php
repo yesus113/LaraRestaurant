8dff1b09699f6bd8ea40e3f9d4d02b9b47ac42ed
Adrian JG <jesusadrianjeronimo113@gmail.com>
2025-12-28 13:06:16 -0600

    refactoring button-edit

--- resources/views/components/buttons/button-edit.blade.php
+++ resources/views/components/buttons/button-edit.blade.php
@@ -1,4 +1,8 @@
+@props([
+        'route',
+        'name' => 'empty' //default value bro, don't worry
+        ])
 <button
     class=" glass border border-blue-600 text-white hover:bg-yellow-600 hover:text-white font-medium px-4 py-2 rounded-md transition m-2">
-    @yield('ruta1')
+    <a href="{{ $route }}">{{ $name }}</a>
 </button>
