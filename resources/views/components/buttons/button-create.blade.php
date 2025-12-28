a9af1e9652bd478cd0274139ce52a6932adb6354
Adrian JG <jesusadrianjeronimo113@gmail.com>
2025-12-28 12:30:51 -0600

    modified: Refectarizacion button-create

--- resources/views/components/buttons/button-create.blade.php
+++ resources/views/components/buttons/button-create.blade.php
@@ -1,4 +1,8 @@
+    @props([
+    'route',
+    'name' => 'empty',
+])
     <button
         class=" glass border border-blue-600 text-white hover:bg-green-600 hover:text-white font-medium px-4 py-2 rounded-md transition m-3">
-        @yield('ruta')
+        <a href="{{ $route }}">{{ $name }}</a>
     </button>
