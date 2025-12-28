7351b7ea7c722b92f7cdc3ef141fe95348ecba52
Adrian JG <jesusadrianjeronimo113@gmail.com>
2025-12-28 13:01:24 -0600

    Just a message

--- resources/views/components/buttons/button-create.blade.php
+++ resources/views/components/buttons/button-create.blade.php
@@ -1,7 +1,7 @@
     @props([
-    'route',
-    'name' => 'empty',
-])
+        'route',
+        'name' => 'empty' //default value bro, don't worry
+        ])
     <button
         class=" glass border border-blue-600 text-white hover:bg-green-600 hover:text-white font-medium px-4 py-2 rounded-md transition m-3">
         <a href="{{ $route }}">{{ $name }}</a>
