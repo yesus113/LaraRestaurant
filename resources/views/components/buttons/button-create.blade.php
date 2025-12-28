b71facbfeb74cd52952339df0eb0278c25b63926
Emmanuel Ramirez <161186127+manolitobes@users.noreply.github.com>
2025-12-27 23:47:37 -0600

    create folder components buttons

---
+++ resources/views/components/buttons/button-create.blade.php
@@ -0,0 +1,4 @@
+    <button
+        class=" glass border border-blue-600 text-white hover:bg-green-600 hover:text-white font-medium px-4 py-2 rounded-md transition m-3">
+        @yield('ruta')
+    </button>
---
+++ resources/views/components/buttons/button-edit.blade.php
@@ -0,0 +1,4 @@
+<button
+    class=" glass border border-blue-600 text-white hover:bg-yellow-600 hover:text-white font-medium px-4 py-2 rounded-md transition m-2">
+    @yield('ruta1')
+</button>
