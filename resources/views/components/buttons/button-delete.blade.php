f552aa9a6450f8b3c9eabe43365d4428a2b55411
Adrian JG <jesusadrianjeronimo113@gmail.com>
2025-12-28 13:02:59 -0600

    refactoring button-delete

--- resources/views/components/buttons/button-delete.blade.php
+++ resources/views/components/buttons/button-delete.blade.php
@@ -1,8 +1,12 @@
-@yield('route-del') {{-- <form method="POST" action="{{ route('dish.destroy', $dish->id) }}"> --}}
+@props([
+        'action',
+        'name' => 'empty' //default value bro, don't worry
+        ])
+<form method="POST" action="{{ $action }}">
     @csrf
     @method('DELETE')
     <button type="submit" value="DELETE"
         class=" glass border border-blue-600 text-white hover:bg-red-600 hover:text-white font-medium px-4 py-2 rounded-md transition m-2">
-        Borrar
+        {{ $name }}
     </button>
 </form>
