1db18429096d95bad91a8184147d0f1900f9e476
Emmanuel Ramirez <161186127+manolitobes@users.noreply.github.com>
2025-12-29 00:13:31 -0600

    create: new folder for alerts components

---
+++ resources/views/components/alerts/error-alert.blade.php
@@ -0,0 +1,9 @@
+@if ($errors->any())
+    <div class="alert alert-error mb-4">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>• {{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
