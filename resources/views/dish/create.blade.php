75d01ecc52ecc3eba92714967db80eade317daf5
Adrian JG <jesusadrianjeronimo113@gmail.com>
2025-12-29 18:55:15 -0600

    added errors message and h2 style

--- resources/views/dish/create.blade.php
+++ resources/views/dish/create.blade.php
@@ -1,35 +1,46 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2>Create view</h2>
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight"> {{ 'Crear' }}</h2>
     </x-slot>
     <div class="flex justify-center mt-10">
         <div class="card w-96 bg-base-100 shadow-xl">
             <div class="card-body">
                 <form action="{{ route('dish.store') }}" method="POST">
                     @csrf
-                    <label class="input m-2">
-                        <span class="label">Nombre</span>
-                        <input class="text-black" type="text" placeholder="Enchiladas Verde" name="name"
-                            value="{{ old('name') }}" />
-                    </label>
+                    <div>
+                        <label class="input m-1">
+                            <span class="label">Nombre</span>
+                            <input class="text-black" type="text" placeholder="Enchiladas Verde" name="name"
+                                value="{{ old('name') }}" />
+                        </label>
+                        <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                    </div>
 
-                    <label class="input m-2">
-                        <span class="label">Descripcion</span>
-                        <input class="text-black" type="text" placeholder="Ingredientes" name="description"
-                            value="{{ old('description') }}" />
-                    </label>
+                    <div>
+                        <label class="input m-2">
+                            <span class="label">Descripcion</span>
+                            <input class="text-black" type="text" placeholder="Ingredientes" name="description"
+                                value="{{ old('description') }}" />
+                        </label>
+                        <x-input-error class="mt-2" :messages="$errors->get('description')" />
+                    </div>
 
-                    <label class="input m-2">
+                    <div>
+                        <label class="input m-2">
                         <span class="label">Precio</span>
                         <input class="text-black" type="text" placeholder="100" name="price"
                             value="{{ old('price') }}" />
                     </label>
+                    <x-input-error class="mt-2" :messages="$errors->get('price')" />
+                    </div>
+
 
                     <label class="select">
                         <span class="label">Categoria</span>
                         <select class="text-black" name="category_id">
                             @forelse ($categories as $category)
-                                <option {{ old('category_id') == $category->id ? 'selected' : '' }} value="{{ $category->id }}">
+                                <option {{ old('category_id') == $category->id ? 'selected' : '' }}
+                                    value="{{ $category->id }}">
                                     {{ $category->name }}
                                 </option>
                             @empty
