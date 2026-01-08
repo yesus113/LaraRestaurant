cc2035e0b2bda9e02c2cccc4b2a79cc7e1d2a4e4
Emmanuel Ramirez <161186127+manolitobes@users.noreply.github.com>
2026-01-07 23:09:28 -0600

    modified: new component messages

--- resources/views/components/alerts/messages.blade.php
+++ resources/views/components/alerts/messages.blade.php
@@ -1,11 +1,17 @@
 @if ($message = Session::get('success'))
-    <div style="padding: 15px; background-color: green; color: white;">
+    <div style="padding: 15px; background-color: green; color: black;">
         <p>{{$message}}</p>
     </div>
 @endif
 
 @if ($message = Session::get('danger'))
-    <div style="padding: 15px; background-color: red; color: white;">
+    <div style="padding: 15px; background-color: red; color: black;">
+        <p>{{$message}}</p>
+    </div>
+@endif
+
+@if ($message = Session::get('update'))
+    <div style="padding: 15px; background-color: yellow; color: black;">
         <p>{{$message}}</p>
     </div>
 @endif
