12ba7dac58760244a412edc860409bb4d717bbad
manolitobes <manolitobes@gmail.com>
2026-01-30 23:19:54 -0600

    create: button document

---
+++ resources/views/components/buttons/button-back.blade.php
@@ -0,0 +1,15 @@
+@props(['route', 'name' => 'empty'])
+
+<a href="{{ $route }}" 
+   class="bg-white text-center w-48 rounded-2xl h-14 relative text-black text-xl font-semibold group flex items-center justify-center m-3">
+    
+    <div class="bg-green-400 rounded-xl h-12 w-1/4 flex items-center justify-center absolute left-1 top-[4px] group-hover:w-[184px] z-10 duration-500">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" height="25px" width="25px">
+            <path d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z" fill="#000000"></path>
+            <path d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z" fill="#000000"></path>
+        </svg>
+    </div>
+
+    {{ $name }}
+</a>
+
