d953a8aa03a41b04429125aa8dccac57debbcd8e
Adrian Jeronimo <developer.jeronimo@qasar.app>
2026-02-03 00:37:08 -0600

    feat(buttons) add confirmation message

--- resources/views/components/buttons/button-delete.blade.php
+++ resources/views/components/buttons/button-delete.blade.php
@@ -6,7 +6,8 @@
     @csrf
     @method('DELETE')
     <button type="submit" value="DELETE"
-        class=" glass border border-blue-600 text-white hover:bg-red-600 hover:text-white font-medium px-4 py-2 rounded-md transition m-2">
+        class=" glass border border-blue-600 text-white hover:bg-red-600 hover:text-white font-medium px-4 py-2 rounded-md transition m-2"
+        onclick="return confirm('¿Estás seguro de que deseas eliminar este registro?')"> {{--mesanje de confimacion bru, too easy LMAO --}}
         {{ $name }}
     </button>
 </form>
