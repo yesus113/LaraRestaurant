16da621e91ca72771fb38813e85d933411e588ea
manolitobes <manolitobes@gmail.com>
2026-04-05 17:33:16 -0600

    modified: adaptacion de los botones al cambio de tema oscuro/claro

--- resources/views/components/buttons/button-create.blade.php
+++ resources/views/components/buttons/button-create.blade.php
@@ -1,8 +1,8 @@
     @props([
         'route',
-        'name' => 'empty' //default value bro, don't worry
-        ])
+        'name' => 'empty', //default value bro, don't worry
+    ])
     <button
-        class=" glass border border-blue-600 text-white hover:bg-green-600 hover:text-white font-medium px-4 py-2 rounded-md transition m-3 mx-auto block">
+        class="glass border border-blue-600 text-base-content hover:bg-primary hover:text-primary-content font-medium px-4 py-2 rounded-md transition m-3 mx-auto block">
         <a href="{{ $route }}">{{ $name }}</a>
     </button>
--- resources/views/components/buttons/button-edit.blade.php
+++ resources/views/components/buttons/button-edit.blade.php
@@ -3,6 +3,6 @@
         'name' => 'empty' //default value bro, don't worry
         ])
 <button
-    class=" glass border border-blue-600 text-white hover:bg-yellow-600 hover:text-white font-medium px-4 py-2 rounded-md transition m-2">
+    class=" glass border border-blue-600 text-base-content hover:bg-yellow-600 hover:primary-content font-medium px-4 py-2 rounded-md transition m-2 block">
     <a href="{{ $route }}">{{ $name }}</a>
 </button>
