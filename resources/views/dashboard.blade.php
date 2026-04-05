f3a5951f0ec93992eef5015f22b0caf17d3eb196
Adrian Jeronimo <developer.jeronimo@qasar.app>
2026-04-04 21:39:45 -0600

    feat(dashboard): use reusable card component

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,57 +11,32 @@
                     {{ __("You're logged in!") }}
                 </div>
 
+                <!-- Options-->
                 <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+                    <x-dashboard.a-card
+                        route="{{ route('dish.index', ['category' => 'entrada']) }}"
+                        imgUrl="{{ asset('img/entrada/entrada.jpg') }}"
+                        alt="Entrada"
+                        name="Entrada"/>
 
-                    <a href="{{ route('dish.index', ['category' => 'entrada']) }}">
-                        <div class="card p-1 rounded h-72 overflow-hidden">
-                            <figure class="px-4 pt-4 h-40">
-                                <img src="{{ asset('img/entrada/entrada.jpg') }}" alt="Entrada" class="rounded-xl" />
-                            </figure>
-                            <div class="card-body items-center text-center">
-                                <h2 class="card-title">Entradas</h2>
-                            </div>
-                        </div>
-                    </a>
+                    <x-dashboard.a-card
+                        route="{{ route('dish.index', ['category' => 'platillo fuerte']) }}"
+                        imgUrl="{{ asset('img/pf/picaña.jpg') }}"
+                        alt="Platillo Fuerte"
+                        name="Platillo Fuerte"/>
 
-                    <a href="{{ route('dish.index', ['category' => 'platillo fuerte']) }}">
-                        <div class="card p-1 rounded h-72 overflow-hidden">
-                            <figure class="px-4 pt-4 h-40">
-                                <img src="{{ asset('img/pf/picaña.jpg') }}" alt="Platillo Fuerte"
-                                    class="rounded-xl " />
-                            </figure>
+                    <x-dashboard.a-card
+                        route="{{ route('dish.index', ['category' => 'postre']) }}"
+                        imgUrl="{{ asset('img/postre/cupcakes-de-chocolate.jpg') }}"
+                        alt="Postre"
+                        name="Postre"/>
 
-                            <div class="card-body items-center text-center">
-                                <h2 class="card-title">Platillos Fuerte</h2>
-                            </div>
-                        </div>
-                    </a>
-
-                    <a href="{{ route('dish.index', ['category' => 'postre']) }}">
-                        <div class="card p-1 rounded h-72 overflow-hidden">
-                            <figure class="px-4 pt-4 h-40">
-                                <img src="{{ asset('img/postre/cupcakes-de-chocolate.jpg') }}" alt="Postres"
-                                    class="rounded-xl" />
-                            </figure>
-                            <div class="card-body items-center text-center">
-                                <h2 class="card-title">Postres</h2>
-                            </div>
-                        </div>
-                    </a>
-
-                    <a href="{{ route('dish.index', ['category' => 'bebida']) }}">
-                        <div class="card p-1 rounded h-72 overflow-hidden">
-                            <figure class="px-4 pt-4 h-40">
-                                <img src="{{ asset('img/bebida/bebidas-naturales.jpg') }}" alt="Bebidas"
-                                    class="rounded-xl" />
-                            </figure>
-                            <div class="card-body items-center text-center">
-                                <h2 class="card-title">Bebidas</h2>
-                            </div>
-                        </div>
-                    </a>
+                    <x-dashboard.a-card
+                        route="{{ route('dish.index', ['category' => 'bebida']) }}"
+                        imgUrl="{{ asset('img/bebida/bebidas-naturales.jpg') }}"
+                        alt="Bebida"
+                        name="Bebida"/>
                 </div>
-
             </div>
         </div>
     </div>
