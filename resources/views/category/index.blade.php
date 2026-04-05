4077d9a0e869fb517b040c125a193f256500f1eb
manolitobes <manolitobes@gmail.com>
2026-04-05 17:31:22 -0600

    modified: actualizacion de la vista

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -5,33 +5,34 @@
 
     <x-alerts.messages />
 
-    <x-buttons.button-back route="{{ route('dashboard') }}" name="Back" />
+    <x-buttons.button-back route="{{ route('dashboard') }}" name="Back" class="bg-blue-400" />
 
-    <div class="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+    <div class="overflow-x-auto rounded-lg border border-base-300 shadow-sm p-6">
+        <br>
         <table class="table">
             <!-- head -->
-            <thead>
+            <thead class="bg-base-200">
                 <tr>
                     <th>Nombre</th>
                 </tr>
             </thead>
             <tbody>
                 @forelse ($categories as $category)
-                    <tr>
-                        <th>{{ $category->name }}</th>
-                        <th>
-                            <x-buttons.button-edit route="{{ route('categ.edit', $category->id) }}" name="Editar" />
-
-                            <x-buttons.button-delete action="{{ route('categ.destroy', $category->id) }}"
-                                name="Borrar" />
-                        </th>
+                    <tr class="hover:bg-base-200/50 transition-colors">
+                        <td>{{ $category->name }}</td>
+                        <td>
+                            <div class="flex items-center justify-center gap-2">
+                                <x-buttons.button-edit route="{{ route('categ.edit', $category->id) }}" name="Editar" />
+                                <x-buttons.button-delete action="{{ route('categ.destroy', $category->id) }}"
+                                    name="Borrar" />
+                            </div>
+                        </td>
                     </tr>
                 @empty
                     <tr>
-                        <th>No data</th>
-                        <th>No data</th>
-                        <th>No data</th>
-                        <th>No data</th>
+                        <td colspan="5" class="text-center py-10 text-base-content/50 italic">
+                            Sin categorias
+                        </td>
                     </tr>
                 @endforelse
             </tbody>
