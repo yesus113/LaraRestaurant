39290bdc65b82f357dd7950029ad31ba0c1b926e
Adrian Jeronimo <developer.jeronimo@qasar.app>
2026-05-01 22:46:41 -0600

    migration: add imageUrl column to dish table

---
+++ database/migrations/2026_05_02_042434_update_dishes_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('dishes', function (Blueprint $table) {
+            $table->text('imageUrl')->nullable(); //* columna para imagines por platillo
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
